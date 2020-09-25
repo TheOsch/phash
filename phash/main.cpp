1a024ad679dff0b5f2cbde8db584852ae12c29a8
Vyacheslav V Sokolov <deathless@t-sk.ru>
2020-09-25 10:17:13 +0700

    Initial Commit

---
+++ phash/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  phash
+//
+//  Created by Vyacheslav V Sokolov on 25/09/2020.
+//  Copyright © 2020 Vyacheslav V Sokolov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
